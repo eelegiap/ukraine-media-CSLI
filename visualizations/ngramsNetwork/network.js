ffb5d02546f79a07cbd2549925803d8f42af4135
Paige Lee <paigelee@DN0a1f624c.SUNet>
2022-08-02 15:01:41 -0700

    visualizations

---
+++ visualizations/ngramsNetwork/network.js
@@ -0,0 +1,218 @@
+// https://github.com/d3/d3-scale-chromatic#schemeRdBu
+
+function draw(category) {
+
+    d3.selectAll('svg').remove()
+
+    var width = screen.width,
+        height = screen.height * 1.25;
+
+    var color = d3.scale.category20();
+    var origLinkOpacity = .6
+    var force = d3.layout.force()
+        .charge(-15)
+        .linkDistance(150)
+        .size([width, height]);
+
+    var x = d3.scale.linear()
+        .domain([0, 500])
+        .range([400, 100])
+        .clamp(true);
+
+    var brush = d3.svg.brush()
+        .y(x)
+        .extent([0, 0]);
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var links_g = svg.append("g");
+
+    var nodes_g = svg.append("g");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(" + (width - 200) + ",0)")
+        .call(d3.svg.axis()
+            .scale(x)
+            .orient("left")
+            .tickFormat(function (d) { return d; })
+            .tickSize(3)
+            .tickPadding(12))
+        .select(".domain")
+        .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "halo");
+
+    var slider = svg.append("g")
+        .attr("class", "slider")
+        .call(brush);
+
+    slider.selectAll(".extent,.resize")
+        .remove();
+
+    var handle = slider.append("circle")
+        .attr("class", "handle")
+        .attr("transform", "translate(" + (width - 200) + ",0)")
+        .attr("r", 5);
+
+    svg.append("text")
+        .attr("x", width - 195)
+        .attr("y", 80)
+        .attr("text-anchor", "end")
+        .attr("font-size", "12px")
+        .style("opacity", 0.75)
+        .text("co-occurrence threshold")
+
+    d3.json(`${category}ngramNet4.json`, function (error, graph) {
+        if (error) throw error;
+
+        graph.links.forEach(function (d, i) { d.i = i; });
+
+        function brushed() {
+            var value = brush.extent()[0];
+
+            if (d3.event.sourceEvent) {
+                value = x.invert(d3.mouse(this)[1]);
+                brush.extent([value, value]);
+            }
+            handle.attr("cy", x(value));
+            var threshold = value;
+
+            var thresholded_links = graph.links.filter(function (d) { return (d.value > threshold); });
+
+            force
+                .links(thresholded_links);
+
+            var link = links_g.selectAll(".link")
+                .data(thresholded_links, function (d) { return d.i; });
+
+            link.enter().append("line")
+                .attr("class", "link")
+                .style('stroke-opacity', origLinkOpacity)
+                .style("stroke-width", function (d) { console.log(d.value); return Math.sqrt(Math.sqrt(d.value)); });
+
+            link.exit().remove();
+
+            force.on("tick", function () {
+                link.attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
+
+                node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+                // .attr("cx", function (d) { return d.x; })
+                // .attr("cy", function (d) { return d.y; });
+            });
+            force.start();
+
+            // get thresholded nodes
+            var validIDs = []
+            thresholded_links.forEach(function(tl) {
+                var sourceID = tl.source.id
+                var targetID = tl.target.id
+                validIDs.push(sourceID)
+                validIDs.push(targetID)
+            })
+            // go through all nodes, remove if not connected to thresholded link
+            d3.selectAll('.node').each(function (n) {
+                var thisNode = d3.select(this)
+                var nodeID = thisNode.attr('id')
+                if (!validIDs.includes(nodeID)) {
+
+                    thisNode.transition(200).style('opacity',0)
+                    thisNode.classed('hide',true)
+                } else {
+                    thisNode.transition(200).style('opacity',1)
+                    thisNode.transition(250).style('pointer-events','all')
+                    thisNode.classed('hide',false)
+                }
+            })
+        }
+
+        force
+            .nodes(graph.nodes);
+
+        var node = nodes_g.selectAll(".node")
+            .data(graph.nodes)
+            .enter()
+            .append("g")
+            .attr("class", "node")
+            .attr('id', d => d.id)
+            .each(function (d) {
+                d3.select(this)
+                    .append("circle")
+                    .attr("r", function (d) {
+                        return Math.log(d.occurrences)
+                    })
+                    .style("fill", function (d) {
+                        return 'gray'
+                    }).style('opacity',.75)
+                d3.select(this)
+                    .append("text").text(d.id)
+                    .attr("dy", 5 + 10)
+                    .style("text-anchor", "middle");
+            })
+            .call(force.drag);
+
+        d3.selectAll('.node').on('mouseover', function (d, i) {
+            var nodeID = d.id
+            var connectedIDs = new Set()
+            connectedIDs.add(nodeID)
+            // highlight/unhighlight links
+            d3.selectAll('.link').each(function (l) {
+                var thisLink = d3.select(this)
+                var sourceID = l.source.id
+                var targetID = l.target.id
+                if (nodeID == sourceID || nodeID == targetID) {
+                    thisLink.transition().style('stroke-opacity', origLinkOpacity)
+                    connectedIDs.add(sourceID)
+                    connectedIDs.add(targetID)
+                } else {
+                    thisLink.transition().style('stroke-opacity', .1)
+                }
+            })
+            // highlight/unhighlight circle and text
+            d3.selectAll('.node').each(function () {
+                var thisNode = d3.select(this)
+                var thisNodeID = d3.select(this).attr('id')
+                var thisNodeClasses = thisNode.attr('class')
+                if (connectedIDs.has(thisNodeID)) {
+                    if (!thisNodeClasses.includes('hide')) {
+                    thisNode.transition().style('opacity', 1)
+                    }
+                } else {
+                    thisNode.transition().style('opacity', .1)
+                }
+            })
+        }).on('mouseout', function () {
+            d3.selectAll('.link').transition().style('stroke-opacity', origLinkOpacity)
+            d3.selectAll('.node').transition().style('opacity', 1)
+        })
+
+
+        brush.on("brush", brushed);
+
+        slider
+            .call(brush.extent([100, 100]))
+            .call(brush.event);
+
+    });
+}
+
+$(document).ready(function () {
+    console.log("ready!");
+    draw('western')
+    d3.select('#category').on('change', function(d) {
+        console.log('dropdown changed')
+        draw(d3.select(this).node().value)
+    })
+    // $('#toggle').change(function () {
+    //     console.log($(this).prop('checked'))
+    //     if ($(this).prop('checked')) {
+    //         draw('notPEN')
+    //     } else {
+    //         draw('PEN')
+    //     }
+    // })
+});
---
+++ visualizations/timeline/timeline.js
@@ -0,0 +1,334 @@
+// https://bl.ocks.org/larsenmtl/e3b8b7c2ca4787f77d78f58d41c3da91
+function draw(separated, topicsOfInterest, topicData) {
+    var checkboxes = d3.selectAll('#checkboxDiv input')
+
+    checkboxes.each(function (d, i) {
+        if (d3.select(this).property('checked')) {
+            topicsOfInterest.push(i)
+        }
+    })
+    topicsOfInterest.sort(function(a, b) {
+        return a - b;
+      });      
+
+    var margin = {
+        top: 20,
+        right: 200,
+        bottom: 30,
+        left: 50
+    },
+        width = $('#timelineParent').width() - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%Y%m%d").parse;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var color = d3.scale.category20();
+
+    // time stuff
+    var ticks = ['20211001',
+    '20211016',
+    '20211101',
+    '20211116',
+    '20211201',
+    '20211216',
+    '20220101',
+    '20220116',
+    '20220201',
+    '20220216']
+    var formattedTicks = ticks.map(d => parseDate(d))
+    var formatDate = d3.time.format("%b-%e-%y")
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickValues(formattedTicks)
+        .tickFormat(formatDate)
+        
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.temperature);
+        });
+
+    $('#timeline').empty()
+
+    var svg = d3.select("#timeline").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json('freq_data.json', function (jsonData) {
+
+        var data = jsonData.map(function (d) {
+            var datumObj = Object()
+            datumObj['date'] = d.date;
+            topicsOfInterest.map(function (t) {
+                // datumObj[topicData[t+1].description] = d[`topic_${t}`]
+                if (separated) {
+                    datumObj[`${topicData[t + 1].description} (Russian)`] = d[`russian_freq_topic_${t}`]
+                    datumObj[`${topicData[t + 1].description} (Western)`] = d[`western_freq_topic_${t}`]
+                } else {
+                    datumObj[`${topicData[t + 1].description} (All)`] = d[`total_freq_topic_${t}`]
+                }
+            })
+            return datumObj
+        })
+
+        color.domain(d3.keys(data[0]).filter(function (key) {
+            return key !== "date";
+        }));
+
+        data.forEach(function (d) {
+            d.date = parseDate(d.date);
+        });
+
+        var cities = color.domain().map(function (name) {
+            return {
+                name: name,
+                values: data.map(function (d) {
+                    return {
+                        date: d.date,
+                        temperature: +d[name]
+                    };
+                })
+            };
+        });
+
+        x.domain(d3.extent(data, function (d) {
+            return d.date;
+        }));
+
+        y.domain([
+            d3.min(cities, function (c) {
+                return d3.min(c.values, function (v) {
+                    return v.temperature;
+                });
+            }),
+            d3.max(cities, function (c) {
+                return d3.max(c.values, function (v) {
+                    return v.temperature;
+                });
+            })
+        ]);
+
+        var legend = svg.selectAll('g')
+            .data(cities)
+            .enter()
+            .append('g')
+            .attr('class', 'legend');
+
+        legend.append('rect')
+            .attr('x', 50)
+            .attr('y', function (d, i) {
+                return i * 20;
+            })
+            .attr('width', 10)
+            .attr('height', 10)
+            .style('fill', function (d) {
+                return color(d.name);
+            });
+
+        legend.append('text')
+            .attr('x', 66)
+            .attr('y', function (d, i) {
+                return (i * 20) + 9;
+            })
+            .text(function (d) {
+                return d.name;
+            }).attr('font-size', '12px');
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Frequency of sentences in topic")
+
+        var city = svg.selectAll(".city")
+            .data(cities)
+            .enter().append("g")
+            .attr("class", "city");
+
+        city.append("path")
+            .attr("class", "line")
+            .attr("d", function (d) {
+                return line(d.values);
+            })
+            .style("stroke", function (d) {
+                return color(d.name);
+            });
+
+        city.append("text")
+            .datum(function (d) {
+                return {
+                    name: d.name,
+                    value: d.values[d.values.length - 1]
+                };
+            })
+            .attr("transform", function (d) {
+                return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")";
+            })
+            .attr("x", 3)
+            .attr("dy", ".35em")
+            .text(function (d) {
+                return d.name;
+            }).style('font-size', '10px')
+
+        var mouseG = svg.append("g")
+            .attr("class", "mouse-over-effects");
+
+        mouseG.append("path") // this is the black vertical line to follow mouse
+            .attr("class", "mouse-line")
+            .style("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", "0");
+
+        var lines = document.getElementsByClassName('line');
+
+        var mousePerLine = mouseG.selectAll('.mouse-per-line')
+            .data(cities)
+            .enter()
+            .append("g")
+            .attr("class", "mouse-per-line");
+
+        mousePerLine.append("circle")
+            .attr("r", 7)
+            .style("stroke", function (d) {
+                return color(d.name);
+            })
+            .style("fill", "none")
+            .style("stroke-width", "1px")
+            .style("opacity", "0");
+
+        mousePerLine.append("text")
+            .attr("transform", "translate(10,3)");
+
+        mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+            .attr('width', width) // can't catch mouse events on a g element
+            .attr('height', height)
+            .attr('fill', 'none')
+            .attr('pointer-events', 'all')
+            .on('mouseout', function () { // on mouse out hide line, circles and text
+                d3.select(".mouse-line")
+                    .style("opacity", "0");
+                d3.selectAll(".mouse-per-line circle")
+                    .style("opacity", "0");
+                d3.selectAll(".mouse-per-line text")
+                    .style("opacity", "0");
+            })
+            .on('mouseover', function () { // on mouse in show line, circles and text
+                d3.select(".mouse-line")
+                    .style("opacity", "1");
+                d3.selectAll(".mouse-per-line circle")
+                    .style("opacity", "1");
+                d3.selectAll(".mouse-per-line text")
+                    .style("opacity", "1");
+            })
+            .on('mousemove', function () { // mouse moving over canvas
+                var mouse = d3.mouse(this);
+                d3.select(".mouse-line")
+                    .attr("d", function () {
+                        var d = "M" + mouse[0] + "," + height;
+                        d += " " + mouse[0] + "," + 0;
+                        return d;
+                    });
+
+                d3.selectAll(".mouse-per-line")
+                    .attr("transform", function (d, i) {
+                        console.log(width / mouse[0])
+                        var xDate = x.invert(mouse[0]),
+                            bisect = d3.bisector(function (d) { return d.date; }).right;
+                        idx = bisect(d.values, xDate);
+
+                        var beginning = 0,
+                            end = lines[i].getTotalLength(),
+                            target = null;
+
+                        while (true) {
+                            target = Math.floor((beginning + end) / 2);
+                            pos = lines[i].getPointAtLength(target);
+                            if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                                break;
+                            }
+                            if (pos.x > mouse[0]) end = target;
+                            else if (pos.x < mouse[0]) beginning = target;
+                            else break; //position found
+                        }
+
+                        d3.select(this).select('text')
+                            .text(y.invert(pos.y).toFixed(2));
+
+                        return "translate(" + mouse[0] + "," + pos.y + ")";
+                    });
+            });
+    })
+
+}
+
+$(document).ready(function () {
+    console.log("ready!");
+
+    var topicsOfInterest = [16]
+
+    d3.tsv('topics.tsv', function (topicData) {
+        var checkboxes = d3.select("#checkboxDiv").selectAll("input")
+            .data(topicData.slice(1,))
+            .enter().append('p').append("label").attr('class', 'cbLabel')
+            .text(function (d) { return d.description; })
+            .append("input")
+            .attr("checked", false)
+            .attr("type", "checkbox")
+            .attr("id", function (d, i) { return i; })
+            .attr("for", function (d, i) { return i; });
+        checkboxes.each(function (d, i) {
+            if (topicsOfInterest.includes(i)) {
+                d3.select(this).property('checked', true);
+            } else {
+                d3.select(this).property('checked', false);
+            }
+        })
+        var separated = true
+        draw(separated, topicsOfInterest, topicData)
+        d3.select('#mediasource').on('change', function (d) {
+            console.log('dropdown changed')
+            var separated = (d3.select(this).node().value === 'separated') ? true : false;
+
+            draw(separated, [], topicData)
+        })
+        d3.selectAll('#checkboxDiv input').on('change', function (d,i) {
+            var separated = (d3.select('#mediasource').node().value === 'separated') ? true : false;
+            draw(separated, [], topicData)
+            // var ischecked = d3.select(this).property('checked')
+            // console.log(d3.select(this).attr('id'), ischecked
+    })
+
+    //     if ($(this).prop('checked')) {
+    //         draw('notPEN')
+    //     } else {
+    //         draw('PEN')
+    //     }
+    })
+});
